8786fdf6e1811f45d04a2dbb211c04c5dcd7c247
Andrey Drobakh <drobahandrey7@gmail.com>
2026-04-28 15:56:20 +0500

    changed test for _get_next_index function

--- test_game.py
+++ test_game.py
@@ -3,18 +3,13 @@ from game import _get_next_index, _is_word_correct
 
 
 def test_get_next_index() :
-    current_index = 1
     player_names = ['John', 'Tom', 'Alex', 'Pit']
-    assert _get_next_index( current_index, player_names ) == 2
+
+    assert _get_next_index( 0, player_names ) == 1
+    assert _get_next_index( 1, player_names ) == 2
+    assert _get_next_index( 2, player_names ) == 3
+    assert _get_next_index( 3, player_names ) == 0
 
 
-def test_get_next_i() :
-    current_index = 3
-    player_names = ['John', 'Tom', 'Alex', 'Pit']
-    assert _get_next_index( current_index, player_names ) == 0
-
-
-def test_is_word_correct() :
-    pass
 
 
